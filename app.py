b0f6f8594c6d91fd0f7207d122f57dfcaecd1597
David Falson <david@falson.net>
2023-01-16 18:10:49 -0800

    worked on step 6

--- app.py
+++ app.py
@@ -320,13 +320,18 @@ def homepage():
     - anon users: no messages
     - logged in: 100 most recent messages of followed_users
     """
-
+    
     if g.user:
+        following_ids = [follow.id for follow in g.user.following] + [g.user.id]
+
         messages = (Message
                     .query
+                    .filter(Message.user_id.in_(following_ids))
                     .order_by(Message.timestamp.desc())
                     .limit(100)
                     .all())
+
+
 
         return render_template('home.html', messages=messages)
 
